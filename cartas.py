e905fee978372fc475c815515a7e23b81d11c0a3
Ricardo Almeida <a21807601@alunos.ulht.pt>
2022-11-03 17:14:21 -0200

    Introduced the game mechanics where you alter your hand and hid the cards the Computer gets. Also introduced some loops to make the game flow better and print who wins rather than the resulting number.

--- cartas.py
+++ cartas.py
@@ -1,14 +1,18 @@
 #IMPORTS
 import random
 
-#THIS FUNCTION PRINTS THE SHUFFLED DECKS
+#VARIABLES
+GameLoop = True
+
+#THIS FUNCTION PRINTS THE SHUFFLED DECK OF THE PLAYER AND SIMPLY SHUFFLES THE AI'S DECK
 def PrintShuffled(x,y):
-    print("Player's Deck: " + str(Shuffle(x)))
-    print("Computer's Deck: " + str(Shuffle(y)))
+    Shuffle(x)
+    print("Player's Deck: " + str(x))
+    Shuffle(y)
     print("")
     
 #THIS FUNCTION SHUFFLES THE DECKS
-def Shuffle(x,):
+def Shuffle(x):
     random.shuffle(x)
 
 #THIS FUNCTION RETURNS TWO LISTS, ONE FOR THE LAST TWO CARDS OF EACH DECK
@@ -47,24 +51,118 @@ def transformValues(card):
     else:
         return card
 
+#THIS FUNCTION REPLACES YOUR HAND WITH THE LAST TWO CARDS OF THE DECK
+def Discard2(x):
+    return x[:-3:-1]
 
-#MAIN PROGRAM STARTS HERE
-#DEFINE THE DECK
-DeckPlayer = [1,2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+#THIS FUNCTION LETS YOU CHOOSE WHICH CARD TO DISCARD AND THEN REPLACES THAT ONE WITH THE LAST CARD OF THE DECK
+def Discard1():
+    PlayerChoiceLoop2 = True
+    while PlayerChoiceLoop2 == True:
+        print("")
+        print("Which card do you wish to discard?")
+        Card = input("")
 
-#CREATE A SECOND DECK BY COPYING THE VALUES OF THE FIRST ONE
-DeckAI = DeckPlayer.copy()
+        #THIS TRY IS TO COVER THE CASES OF THE Q,J,K,A
+        try:
+            Card = int(Card)
+        except:
+            pass
+        
+        try:
+            handPlayer.remove(Card)
+            handPlayer.append(DeckPlayer[-1])
+            PlayerChoiceLoop2 = False
+        except:
+            print("This is not a valid option!")
+            print("")
+    return handPlayer
 
-#CALL THE FUNCTION TO PRINT THE SHUFFLED DECKS.
-PrintShuffled(DeckPlayer, DeckAI)
+#THIS FUNCTION SIMPLY LETS YOU PASS AS NO CHANGES NEED TO BE DONE TO EITHER THE DECK OR THE HAND
+def Keep():
+    pass
 
-#CREATE TWO LISTS WITH THE LAST TWO CARDS OF EACH DECK
-handPlayer, handAI = Fish(DeckPlayer, DeckAI)
+#GAME LOOP STARTS HERE
+while GameLoop == True:
+    #DEFINE THE DECK
+    DeckPlayer = [1,2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
 
-#PRINT THE PLAYER'S AND THE COMPUTER'S CARDS
-print("Player's Cards: " + str(handPlayer))
-print("Computer's Cards: " + str(handAI))
-print("")
+    #CREATE A SECOND DECK BY COPYING THE VALUES OF THE FIRST ONE
+    DeckAI = DeckPlayer.copy()
 
-#PRINT THE FINAL RESULT, CALLING THE FUNCTION TO COMPARE THE VALUE OF THE LAST TWO CARDS OF EACH DECK.
-print("Your result is " + str(Compare(handPlayer, handAI)))
+    #CALL THE FUNCTION TO PRINT THE PLAYER'S SHFULLED DECK.
+    PrintShuffled(DeckPlayer, DeckAI)
+
+    #CREATE TWO LISTS WITH THE LAST TWO CARDS OF EACH DECK
+    handPlayer, handAI = Fish(DeckPlayer, DeckAI)
+
+    #REMOVE THE DRAWN CARDS FROM THE PLAYER'S DECK
+    for i in handPlayer:
+        DeckPlayer.remove(i)
+
+    #PRINT THE PLAYER'S AND THE COMPUTER'S CARDS
+    print("Player's Cards: " + str(handPlayer))
+    print("")
+
+    #INITIALIZE THE PLAYER CHOICE LOOP SO IF THE PLAYER HAS A MISTAKE, HE CAN INTRODUCE THE OPTION AGAIN
+    PlayerChoiceLoop = True
+
+    #PRESENT THE CHOICES AND LET THE PLAYER CHOOSE WHICH PLAY HE WANTS
+    while PlayerChoiceLoop == True:
+        print("1: Discartar Tudo!")
+        print("2: Discartar uma das cartas!")
+        print("3: Keep your cards!")
+        print("Which option do you prefer?")
+        PlayerChoice = input("")
+
+        if PlayerChoice == "1":
+            handPlayer = Discard2(DeckPlayer)
+            PlayerChoiceLoop = False
+
+        elif PlayerChoice == "2":
+            Discard1()
+            PlayerChoiceLoop = False
+
+        elif PlayerChoice=="3":
+            Keep()
+            PlayerChoiceLoop = False
+        else:
+            print("That is not a valid choice. Please try again!")
+            print("")
+    
+    print("")
+    print("Now that we're all set, it's time to see who wins!")
+
+    #PRINT BOTH THE PLAYER'S AND THE COMPUTER'S HANDS
+    print("")
+    print("Your cards are " + str(handPlayer))
+    print("The computer's cards are "+ str(handAI))
+
+    #UTILIZING THE COMPARE FUNCTION, COMPARES THE VALUE OF BOTH THE PLAYER AND THE COMPUTER'S HANDS AND PRINTS THE RESULT OF THE GAME
+    if Compare(handPlayer,handAI) == -1:
+        print("You have lost!")
+    elif Compare(handPlayer, handAI) == 1:
+        print("You have won!")
+    else:
+        print("It's a tie!")
+
+    print("")
+
+    #CREATES A NEW LOOP SO THE PLAYER DECIDES IF HE WISHES TO PLAY AGAIN OR NOT
+    PlayerChoiceLoop2 = True
+    while PlayerChoiceLoop2 == True:
+        print("Wanna play again? (Y/N)")
+        PlayerChoice2 = input("")
+
+        if PlayerChoice2.lower() == "y":
+            PlayerChoiceLoop2 = False
+            print("")
+            print("")
+            print("")
+        elif PlayerChoice2.lower() == "n":
+            PlayerChoiceLoop2 = False
+            GameLoop = False
+            print("Have a good one!")
+        else:
+            print("That's not a valid option!")
+            print("")
