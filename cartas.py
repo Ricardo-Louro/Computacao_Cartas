067260809a43642ac71dbbbf4932f4b788279fe8
Ricardo Almeida <a21807601@alunos.ulht.pt>
2022-11-03 15:42:39 -0200

    Added commentary. Removed Shuffle from the main body and instead it made it so that PrintShuffled calls it.

--- cartas.py
+++ cartas.py
@@ -1,17 +1,21 @@
+#IMPORTS
 import random
 
-def Shuffle(x,y):
-    random.shuffle(x)
-    random.shuffle(y)
-
+#THIS FUNCTION PRINTS THE SHUFFLED DECKS
 def PrintShuffled(x,y):
-    print("Player's Deck: " + str(x))
-    print("Computer's Deck: " + str(y))
+    print("Player's Deck: " + str(Shuffle(x)))
+    print("Computer's Deck: " + str(Shuffle(y)))
     print("")
+    
+#THIS FUNCTION SHUFFLES THE DECKS
+def Shuffle(x,):
+    random.shuffle(x)
 
-def PrintFish(x,y):
+#THIS FUNCTION RETURNS TWO LISTS, ONE FOR THE LAST TWO CARDS OF EACH DECK
+def Fish(x,y):
     return x[:-3:-1], y[:-3:-1]
 
+#THIS FUNCTIONS COMPARES THE SUM OF THE DECKS AND RETURNS THE FINAL VALUE
 def Compare(deck1, deck2):
     sum1 = SumDeck(deck1)
     sum2 = SumDeck(deck2)
@@ -23,12 +27,14 @@ def Compare(deck1, deck2):
     else:
         return 0
 
+#THIS FUNCTION SUMS THE VALUE OF THE CARDS IN THE PROVIDED LIST
 def SumDeck(deck):
     sumTotal = 0
     for card in deck:
         sumTotal += transformValues(card)
     return sumTotal
 
+#THIS FUNCTIONS TRANSFORMS THE VALUES OF EACH NON-NUMBER CARD INTO A NUMBERED VALUE
 def transformValues(card):
     if card == "J":
         return 11
@@ -41,16 +47,24 @@ def transformValues(card):
     else:
         return card
 
+
+#MAIN PROGRAM STARTS HERE
+#DEFINE THE DECK
 DeckPlayer = [1,2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+
+#CREATE A SECOND DECK BY COPYING THE VALUES OF THE FIRST ONE
 DeckAI = DeckPlayer.copy()
 
-Shuffle(DeckPlayer, DeckAI)
+#CALL THE FUNCTION TO PRINT THE SHUFFLED DECKS.
 PrintShuffled(DeckPlayer, DeckAI)
 
-handPlayer, handAI = PrintFish(DeckPlayer, DeckAI)
+#CREATE TWO LISTS WITH THE LAST TWO CARDS OF EACH DECK
+handPlayer, handAI = Fish(DeckPlayer, DeckAI)
 
+#PRINT THE PLAYER'S AND THE COMPUTER'S CARDS
 print("Player's Cards: " + str(handPlayer))
 print("Computer's Cards: " + str(handAI))
 print("")
 
+#PRINT THE FINAL RESULT, CALLING THE FUNCTION TO COMPARE THE VALUE OF THE LAST TWO CARDS OF EACH DECK.
 print("Your result is " + str(Compare(handPlayer, handAI)))
